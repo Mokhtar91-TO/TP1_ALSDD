082ab5569f49bdfa263f8b51aecec48ee707503e
Mokhtar91-TO <om_touati@esi.dz>
2025-03-14 17:38:54 +0100

    Trying git

---
+++ TP_ALSDD.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void sortArray(int arr[], int n) {
+    int temp;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    sortArray(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
